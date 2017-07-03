19350bcce6cefea65dad48a5be71de5e390d94e1
marshalldanel <marshalldanel@gmail.com>
2017-07-03 23:56:14 +0000

    Made HTTP request with function

--- download_avatars.js
+++ download_avatars.js
@@ -1,3 +1,30 @@
 var request = require('request');
 
-console.log('Welcome to github!');
+function getReqOptions (path) {
+  return {
+    url: 'https://api.github.com' + path,
+    headers: {
+      'User-Agent': 'pinkElephant'
+    },
+    qs: {
+      access_token: process.env.GITHUB_ACCESS_TOKEN
+    }
+  };
+}
+
+function gitHubApiResponse(path, callback) {
+  request(getReqOptions(path), function (error, response, body) {
+    try {
+      const data = JSON.parse(body);
+      callback(data);
+    } catch (err) {
+      console.log('Failed to parse content body');
+    }
+  });
+}
+
+gitHubApiResponse(`/repos/${process.argv[2]}/${process.argv[3]}/contributors`, (data) => {
+  data.forEach((contributor) => {
+    console.log(contributor.login);
+  });
+});
